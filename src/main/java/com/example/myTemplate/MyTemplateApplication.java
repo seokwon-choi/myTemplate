48bbc8914eeeb91336dbeca5e0bf32205a720d92
최석원 <bluecircle2290@gmail.com>
2023-04-08 14:59:35 +0900

    init

---
+++ src/main/java/com/example/myTemplate/MyTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.example.myTemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyTemplateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/myTemplate/MyTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myTemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyTemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
